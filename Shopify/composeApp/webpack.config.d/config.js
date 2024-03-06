b8747b557527d1a992a3345122971408f6dbbee4
ashwani <ashwani.singh@ninestars.in>
2024-03-06 21:20:17 +0530

    Added webpack production config

--- Shopify/composeApp/webpack.config.d/config.js
+++ Shopify/composeApp/webpack.config.d/config.js
@@ -2,27 +2,30 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 config.optimization = config.optimization || {};
 config.optimization.minimize = true;
-config.devServer.host = "192.168.14.2";
-config.devServer.port = 8080;
-config.mode = "development";
+if(config.mode == "development") {
+    config.devServer.host = "192.168.14.2";
+    config.devServer.port = 8080;
 
-config.devServer.headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
-    "Access-Control-Expose-Headers": "Content-Range, X-Content-Range, X-Total-Count, X-Page-Count, X-Page-Number, X-Page-Size, X-Page-Offset",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Max-Age": "86400"
+    config.devServer.headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+        "Access-Control-Expose-Headers": "Content-Range, X-Content-Range, X-Total-Count, X-Page-Count, X-Page-Number, X-Page-Size, X-Page-Offset",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Max-Age": "86400"
+        };
+
+    config.devServer.proxy = {
+        '/api': {
+            target: 'http://localhost:8081',
+            pathRewrite: {'^/api' : ''},
+            secure: false,
+            changeOrigin: true,
+        },
     };
+}
 
-config.devServer.proxy = {
-    '/api': {
-        target: 'http://localhost:8081',
-        pathRewrite: {'^/api' : ''},
-        secure: false,
-        changeOrigin: true,
-    },
-};
+
 
 
 config.optimization.minimizer = [
